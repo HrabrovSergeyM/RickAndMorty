c80aba20a33cc38984c95549a2fe923ee693844b
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-11-29 12:01:07 +0300

    main: added detail view for episode

--- RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class RMEpisodeDetailViewController: UIViewController {
     
+    private let detailView = RMEpisodeDetailView()
+    
     private let vm: RMEpisodeDetailViewViewModel
 
     init(url: URL?) {
@@ -22,6 +24,13 @@ final class RMEpisodeDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(detailView)
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
         title = "Episode"
     }
 }
